af37182195f6480162cfeb2deab94c18a6a7d368
allen <wysalexwang@gmail.com>
2015-07-27 18:08:09 +0800

    調整編寫方式

--- librarycss/js/checkall.js
+++ librarycss/js/checkall.js
@@ -1,15 +1,10 @@
-function checkall(input1,input2) {
-	var objForm = document.forms[input1];
-	var objLen = objForm.length;
-	for (var iCount = 0; iCount < objLen; iCount++) {
-		if (input2.checked == true) {
-			if (objForm.elements[iCount].type == "checkbox") {
-				objForm.elements[iCount].checked = true;
-			}
+function selectAll(input){
+	var checkItem = document.getElementsByName("checkbox[]");
+	for (var i = 0; i < checkItem.length; i++) {
+		if (input.checked == true) {
+			checkItem[i].checked = true;
 		} else {
-			if (objForm.elements[iCount].type == "checkbox") {
-				objForm.elements[iCount].checked = false;
-			}
+			checkItem[i].checked = false;
 		}
 	}
 }
